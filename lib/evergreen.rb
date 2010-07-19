f59ce4080dbb8f2aa1dd0bdb6631e0bddce2ca34
Niklas Ramhöj <dev+niclas@elabs.se>
2010-07-19 14:45:58 +0200

    Use Rack::Builder for prettier URLs

--- lib/evergreen.rb
+++ lib/evergreen.rb
@@ -14,43 +14,55 @@ module Evergreen
 
   class << self
     def application(root, driver=:serve)
-      Class.new(Sinatra::Base).tap do |app|
-        app.reset!
-        app.class_eval do
-          set :static, true
-          set :root, File.expand_path('evergreen', File.dirname(__FILE__))
-          set :public, File.expand_path(File.join(root, 'public'), File.dirname(__FILE__))
+      Rack::Builder.new do
+        map "/jasmine" do
+          use Rack::Static, :urls => ["/"], :root => File.expand_path('jasmine/lib', File.dirname(__FILE__))
+          run lambda { |env| [404, {}, "No such file"]}
+        end
 
-          use Rack::Static, :urls => ["/lib"], :root => File.expand_path('jasmine', File.dirname(__FILE__))
-          use Rack::Static, :urls => ["/evergreen"], :root => File.dirname(__FILE__)
+        map "/resources" do
+          use Rack::Static, :urls => ["/"], :root => File.expand_path('evergreen/resources', File.dirname(__FILE__))
+          run lambda { |env| [404, {}, "No such file"]}
+        end
 
-          helpers do
-            def url(path)
-              request.env['SCRIPT_NAME'].to_s + path.to_s
+        map "/" do
+          app = Class.new(Sinatra::Base).tap do |app|
+            app.reset!
+            app.class_eval do
+              set :static, true
+              set :root, File.expand_path('evergreen', File.dirname(__FILE__))
+              set :public, File.expand_path(File.join(root, 'public'), File.dirname(__FILE__))
+
+              helpers do
+                def url(path)
+                  request.env['SCRIPT_NAME'].to_s + path.to_s
+                end
+              end
+
+              get '/' do
+                @specs = Spec.all(root)
+                erb :list
+              end
+
+              get '/list' do
+                @specs = Spec.all(root)
+                erb :list
+              end
+
+              get '/run/*' do |name|
+                @spec = Spec.new(root, name)
+                @js_spec_helper = Spec.new(root, 'spec_helper.js')
+                @coffee_spec_helper = Spec.new(root, 'spec_helper.coffee')
+                @driver = driver
+                erb :spec
+              end
+
+              get '/spec/*' do |name|
+                Spec.new(root, name).read
+              end
             end
           end
-
-          get '/' do
-            @specs = Spec.all(root)
-            erb :list
-          end
-
-          get '/list' do
-            @specs = Spec.all(root)
-            erb :list
-          end
-
-          get '/run/*' do |name|
-            @spec = Spec.new(root, name)
-            @js_spec_helper = Spec.new(root, 'spec_helper.js')
-            @coffee_spec_helper = Spec.new(root, 'spec_helper.coffee')
-            @driver = driver
-            erb :spec
-          end
-
-          get '/spec/*' do |name|
-            Spec.new(root, name).read
-          end
+          run app
         end
       end
     end
