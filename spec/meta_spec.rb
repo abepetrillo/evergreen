7709892ac218be467bf2ea725df37619ef00b4b7
Niklas Ramhöj <dev+niclas@elabs.se>
2010-07-21 10:09:52 +0200

    simplified meta spec

--- spec/meta_spec.rb
+++ spec/meta_spec.rb
@@ -2,21 +2,25 @@ require 'spec_helper'
 
 describe Evergreen::Runner do
   subject { Evergreen::Runner.new(spec) }
-  let(:root) { File.expand_path('suite1', File.dirname(__FILE__)) }
+  let(:spec) { Evergreen::Spec.new(root, template) }
 
-  context "with transactions spec" do
-    let(:spec) { Evergreen::Spec.new(root, 'transactions_spec.js') }
-    it { should pass }
-  end
+  context "with standard setup" do
+    let(:root) { File.expand_path('suite1', File.dirname(__FILE__)) }
 
-  context "with spec helper" do
-    let(:spec) { Evergreen::Spec.new(root, 'with_helper_spec.js') }
-    it { should pass }
-  end
+    context "with transactions spec" do
+      let(:template) { 'transactions_spec.js' }
+      it { should pass }
+    end
 
-  context "with template spec" do
-    let(:spec) { Evergreen::Spec.new(root, 'templates_spec.js') }
-    it { should pass }
+    context "with spec helper" do
+      let(:template) { 'with_helper_spec.js' }
+      it { should pass }
+    end
+
+    context "with template spec" do
+      let(:template) { 'templates_spec.js' }
+      it { should pass }
+    end
   end
 end
 
