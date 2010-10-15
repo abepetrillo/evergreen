3857314a053f630ee4913856eb81ce9f7200c3c8
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-10-15 15:30:02 +0200

    Move application method into own file

---
+++ lib/evergreen/application.rb
@@ -0,0 +1,56 @@
+module Evergreen
+  def self.application(suite, driver)
+    Rack::Builder.new do
+      map "/jasmine" do
+        use Rack::Static, :urls => ["/"], :root => File.expand_path('../jasmine/lib', File.dirname(__FILE__))
+        run lambda { |env| [404, {}, "No such file"]}
+      end
+
+      map "/resources" do
+        use Rack::Static, :urls => ["/"], :root => File.expand_path('resources', File.dirname(__FILE__))
+        run lambda { |env| [404, {}, "No such file"]}
+      end
+
+      map "/" do
+        app = Class.new(Sinatra::Base).tap do |app|
+          app.reset!
+          app.class_eval do
+            set :static, true
+            set :root, File.expand_path('.', File.dirname(__FILE__))
+            set :public, File.expand_path(File.join(suite.root, 'public'), File.dirname(__FILE__))
+
+            helpers do
+              def url(path)
+                request.env['SCRIPT_NAME'].to_s + path.to_s
+              end
+            end
+
+            get '/' do
+              @suite = suite
+              erb :list
+            end
+
+            get '/list' do
+              @suite = suite
+              erb :list
+            end
+
+            get '/run/*' do |name|
+              @suite = suite
+              @spec = suite.get_spec(name)
+              @js_spec_helper = suite.get_spec('spec_helper.js')
+              @coffee_spec_helper = suite.get_spec('spec_helper.coffee')
+              @driver = driver
+              erb :spec
+            end
+
+            get '/spec/*' do |name|
+              suite.get_spec(name).read
+            end
+          end
+        end
+        run app
+      end
+    end
+  end
+end
