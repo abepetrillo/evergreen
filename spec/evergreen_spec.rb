de7a29aeb2796bbf309041883495510a6b8437fb
Abe Petrillo <abe.petrillo@gmail.com>
2015-03-17 11:21:38 -0400

    update spec files to use expect instead of should

--- spec/evergreen_spec.rb
+++ spec/evergreen_spec.rb
@@ -6,34 +6,34 @@ describe Evergreen::Application do
   it "should show a successful test run" do
     visit("/")
     click_link("testing_spec.js")
-    page.should have_content("2 specs, 0 failures")
+    expect(page).to have_content "2 specs, 0 failures"
   end
 
   it "should show a successful test run for a coffeescript spec" do
     visit("/")
     click_link("coffeescript_spec.coffee")
-    page.should have_content("2 specs, 0 failures")
+    expect(page).to have_content("2 specs, 0 failures")
   end
 
   it "should show errors for a failing spec" do
     visit("/")
     click_link("failing_spec.js")
-    page.should have_content("2 specs, 1 failure")
-    page.should have_content("Expected 'bar' to equal 'noooooo'.")
+    expect(page).to have_content("2 specs, 1 failure")
+    expect(page).to have_content("Expected 'bar' to equal 'noooooo'.")
   end
 
   it "should run all specs" do
     visit("/")
     click_link("All")
-    page.should have_content("18 specs, 3 failures")
-    page.should have_content("Expected 'bar' to equal 'noooooo'.")
+    expect(page).to have_content("18 specs, 3 failures")
+    expect(page).to have_content("Expected 'bar' to equal 'noooooo'.")
   end
 
   it "should run a spec inline" do
     visit("/")
     within('li', :text => 'testing_spec.js') do
       click_link("Run")
-      page.should have_content('Pass')
+      expect(page).to have_content('Pass')
     end
   end
 
@@ -42,9 +42,9 @@ describe Evergreen::Application do
     within('li', :text => 'failing_spec.js') do
       click_link("Run")
       begin
-        page.should have_content('Fail')
+        expect(page).to have_content('Fail')
       rescue # why you make me sad, Capybara webkit???
-        page.should have_content('Fail')
+        expect(page).to have_content('Fail')
       end
     end
   end
